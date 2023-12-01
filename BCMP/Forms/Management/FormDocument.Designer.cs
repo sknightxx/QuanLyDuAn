8259fd71b76f6e641c182e7332a29a8f4236ec0a
Nguyễn Quốc Anh (Han) <quocanh944@gmail.com>
2023-12-02 01:18:14 +0700

    Fix document btn

--- BCMP/Forms/Management/FormDocument.Designer.cs
+++ BCMP/Forms/Management/FormDocument.Designer.cs
@@ -31,19 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDocument));
             this.pn_containerDocument = new System.Windows.Forms.Panel();
             this.pn_tittle = new System.Windows.Forms.Panel();
+            this.bt_delete = new FontAwesome.Sharp.IconButton();
+            this.bt_download = new FontAwesome.Sharp.IconButton();
+            this.bt_print = new FontAwesome.Sharp.IconButton();
             this.lb_file = new System.Windows.Forms.Label();
+            this.bt_upload = new FontAwesome.Sharp.IconButton();
             this.bt_departmentData = new System.Windows.Forms.Button();
             this.bt_publicData = new System.Windows.Forms.Button();
             this.pn_search = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.txt_search = new System.Windows.Forms.TextBox();
+            this.bt_search = new FontAwesome.Sharp.IconButton();
             this.pn_background = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.bt_delete = new FontAwesome.Sharp.IconButton();
-            this.bt_download = new FontAwesome.Sharp.IconButton();
-            this.bt_print = new FontAwesome.Sharp.IconButton();
-            this.bt_upload = new FontAwesome.Sharp.IconButton();
-            this.bt_search = new FontAwesome.Sharp.IconButton();
             this.pn_tittle.SuspendLayout();
             this.pn_search.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -70,60 +70,6 @@
             this.pn_tittle.Controls.Add(this.bt_publicData);
             this.pn_tittle.Controls.Add(this.pn_search);
             this.pn_tittle.Name = "pn_tittle";
-            // 
-            // lb_file
-            // 
-            resources.ApplyResources(this.lb_file, "lb_file");
-            this.lb_file.Name = "lb_file";
-            this.lb_file.Click += new System.EventHandler(this.lb_file_Click);
-            // 
-            // bt_departmentData
-            // 
-            resources.ApplyResources(this.bt_departmentData, "bt_departmentData");
-            this.bt_departmentData.Name = "bt_departmentData";
-            this.bt_departmentData.UseVisualStyleBackColor = true;
-            this.bt_departmentData.Click += new System.EventHandler(this.bt_departmentData_Click);
-            // 
-            // bt_publicData
-            // 
-            resources.ApplyResources(this.bt_publicData, "bt_publicData");
-            this.bt_publicData.Name = "bt_publicData";
-            this.bt_publicData.UseVisualStyleBackColor = true;
-            this.bt_publicData.Click += new System.EventHandler(this.bt_publicData_Click);
-            // 
-            // pn_search
-            // 
-            resources.ApplyResources(this.pn_search, "pn_search");
-            this.pn_search.Controls.Add(this.panel2);
-            this.pn_search.Controls.Add(this.bt_search);
-            this.pn_search.Name = "pn_search";
-            // 
-            // panel2
-            // 
-            resources.ApplyResources(this.panel2, "panel2");
-            this.panel2.Controls.Add(this.txt_search);
-            this.panel2.Name = "panel2";
-            // 
-            // txt_search
-            // 
-            resources.ApplyResources(this.txt_search, "txt_search");
-            this.txt_search.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.txt_search.Name = "txt_search";
-            this.txt_search.Enter += new System.EventHandler(this.txt_search_Enter);
-            this.txt_search.Leave += new System.EventHandler(this.txt_search_Leave);
-            // 
-            // pn_background
-            // 
-            resources.ApplyResources(this.pn_background, "pn_background");
-            this.pn_background.BackColor = System.Drawing.Color.White;
-            this.pn_background.Controls.Add(this.panel1);
-            this.pn_background.Name = "pn_background";
-            // 
-            // panel1
-            // 
-            resources.ApplyResources(this.panel1, "panel1");
-            this.panel1.Controls.Add(this.pn_containerDocument);
-            this.panel1.Name = "panel1";
             // 
             // bt_delete
             // 
@@ -167,6 +113,12 @@
             this.bt_print.UseVisualStyleBackColor = false;
             this.bt_print.Click += new System.EventHandler(this.bt_print_Click);
             // 
+            // lb_file
+            // 
+            resources.ApplyResources(this.lb_file, "lb_file");
+            this.lb_file.Name = "lb_file";
+            this.lb_file.Click += new System.EventHandler(this.lb_file_Click);
+            // 
             // bt_upload
             // 
             resources.ApplyResources(this.bt_upload, "bt_upload");
@@ -180,6 +132,41 @@
             this.bt_upload.Name = "bt_upload";
             this.bt_upload.UseVisualStyleBackColor = false;
             this.bt_upload.Click += new System.EventHandler(this.bt_upload_Click);
+            // 
+            // bt_departmentData
+            // 
+            resources.ApplyResources(this.bt_departmentData, "bt_departmentData");
+            this.bt_departmentData.Name = "bt_departmentData";
+            this.bt_departmentData.UseVisualStyleBackColor = true;
+            this.bt_departmentData.Click += new System.EventHandler(this.bt_departmentData_Click);
+            // 
+            // bt_publicData
+            // 
+            resources.ApplyResources(this.bt_publicData, "bt_publicData");
+            this.bt_publicData.Name = "bt_publicData";
+            this.bt_publicData.UseVisualStyleBackColor = true;
+            this.bt_publicData.Click += new System.EventHandler(this.bt_publicData_Click);
+            // 
+            // pn_search
+            // 
+            resources.ApplyResources(this.pn_search, "pn_search");
+            this.pn_search.Controls.Add(this.panel2);
+            this.pn_search.Controls.Add(this.bt_search);
+            this.pn_search.Name = "pn_search";
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Controls.Add(this.txt_search);
+            this.panel2.Name = "panel2";
+            // 
+            // txt_search
+            // 
+            resources.ApplyResources(this.txt_search, "txt_search");
+            this.txt_search.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Enter += new System.EventHandler(this.txt_search_Enter);
+            this.txt_search.Leave += new System.EventHandler(this.txt_search_Leave);
             // 
             // bt_search
             // 
@@ -195,6 +182,19 @@
             this.bt_search.IconSize = 30;
             this.bt_search.Name = "bt_search";
             this.bt_search.UseVisualStyleBackColor = false;
+            // 
+            // pn_background
+            // 
+            resources.ApplyResources(this.pn_background, "pn_background");
+            this.pn_background.BackColor = System.Drawing.Color.White;
+            this.pn_background.Controls.Add(this.panel1);
+            this.pn_background.Name = "pn_background";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.pn_containerDocument);
+            this.panel1.Name = "panel1";
             // 
             // FormDocument
             // 
